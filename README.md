1df5d04ef9977dbd131bf5d74f0c18bf4c3161f2
pschlo <70105856+pschlo@users.noreply.github.com>
2026-01-05 13:36:00 +0100

    improved cli; bump version to v1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # windsim
+
+Wind turbine simulation framework.
+
+## Installation
+This package is not currently uploaded to PyPI. Install as follows:
+
+1. Find your release of choice [here](https://github.com/pschlo/windsim/releases)
+2. Copy the link to `windsim-x.x.x.tar.gz`
+3. Run `python -m pip install {link}`
+
+You may also prepend a [direct reference](https://peps.python.org/pep-0440/#direct-references), which might be desirable for a `requirements.txt`.
+
+
+## Building
+The `.tar.gz` file in a release is the [source distribution](https://packaging.python.org/en/latest/glossary/#term-Source-Distribution-or-sdist), which was created from the source code with `uv build --sdist`. [Built distributions](https://packaging.python.org/en/latest/glossary/#term-Built-Distribution) are not provided.
+
+## Usage
+
+Run `windsim noise` to run an exemplary noise simulation.
--- src/windsim/cli.py
+++ src/windsim/cli.py
@@ -7,16 +7,54 @@ setup_logging(
     ]
 )
 from pathlib import Path
+import click
+
 
 log = logging.getLogger(__name__)
 
 
-def main():
-    log.info("Starting")
+@click.group(context_settings={"help_option_names": ["-h", "--help"]})
+def cli():
+    """windsim command line interface."""
+
+
+@cli.command()
+@click.option(
+    "--root",
+    type=click.Path(path_type=Path, file_okay=False, dir_okay=True),
+    default=lambda: Path.cwd() / "resources" / "repository",
+    show_default=True,
+    help="Root directory of the repository.",
+)
+@click.option(
+    "--project",
+    type=str,
+    default="foo",
+    show_default=True,
+    help="Project name.",
+)
+@click.option(
+    "--config",
+    "config_path",
+    type=click.Path(path_type=Path, dir_okay=False, file_okay=True),
+    default=lambda: Path.cwd() / "noise_config.toml",
+    show_default=True,
+    help="Path to the noise config TOML file.",
+)
+def noise(root: Path, project: str, config_path: Path):
+    """Run the noise simulation."""
+    log.info("Starting noise simulation")
     from windsim.models.noise.suggested_plan import run_simulation
 
     run_simulation(
-        root=Path.cwd() / "resources" / "repository",
-        project="foo",
-        config_path=Path.cwd() / "noise_config.toml"
+        root=root,
+        project=project,
+        config_path=config_path,
     )
+
+
+# Placeholder for later:
+@cli.command()
+def shadow():
+    """Run the shadow simulation (not implemented yet)."""
+    raise click.ClickException("Shadow simulation is not implemented yet.")
